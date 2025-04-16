9f925f1ddefee68bdbf9996ec29d14a579dc8b6d
Adam <adam.smith97@btinternet.com>
2025-04-16 13:58:00 +0100

    back to point where router is working

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevTools } from "@tanstack/react-query-devtools";
-
 import PizzaOfTheDay from "../PizzaOfTheDay";
 import Header from "../Header";
 import { CartContext } from "../contexts";
@@ -20,7 +18,6 @@ export const Route = createRootRoute({
           </div>
         </CartContext.Provider>
         <TanStackRouterDevtools />
-        {/* <ReactQueryDevTools /> */}
       </>
     );
   },
